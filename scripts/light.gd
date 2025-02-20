9b453fe23d236307fcaec94a2ae1d6e90cdc1185
shiftfox <thiiagoo912@gmail.com>
2025-02-19 21:14:50 -0300

    Luz diminui de tamanho com o tempo

--- scripts/light.gd
+++ scripts/light.gd
@@ -2,33 +2,15 @@ class_name Light extends Area2D
 
 @export var speed: float
 @export var kill_speed: float
-@export var bullet_prefab: PackedScene
-@export var bullet_speed: float
-@export var bullet_count: int
-@export var min_scale: float
-@export var radius: float
-var target_scale: Vector2
-var max_scale: Vector2
 
-func _ready() -> void:
-	target_scale = scale
-	max_scale = scale
+func _ready():
+	var scene = get_tree().current_scene
+	if "kill_speed" in scene:
+		kill_speed = scene.kill_speed
 
 func _process(delta: float) -> void:
 	position = lerp(position, get_global_mouse_position(), speed * delta)
-	scale = lerp(scale, target_scale, kill_speed * delta)
-	if scale.x <= min_scale:
+	scale -= Vector2.ONE * kill_speed * delta
+	if scale.x <= 0:
 		Particles.spawn("death", self)
 		queue_free()
-	if Input.is_action_just_pressed("destroy_light"):
-		Particles.spawn("hit", self)
-		var angle_step = TAU / bullet_count
-		for i in range(bullet_count):
-			var angle = i * angle_step
-			var pos = position + Vector2(cos(angle), sin(angle)) * radius
-			var bullet = bullet_prefab.instantiate()
-			bullet.position = pos
-			bullet.rotation = angle
-			call_deferred("add_sibling", bullet)
-			bullet.fire(bullet_speed)
-		target_scale = scale - Vector2.ONE * 0.25

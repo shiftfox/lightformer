f35f2281b939ef6262760944ac7683db0dddd8e3
shiftfox <thiiagoo912@gmail.com>
2025-02-22 21:12:36 -0300

    Diálogo

---
+++ scripts/dialog.gd
@@ -0,0 +1,5 @@
+class_name Dialog extends Resource
+
+@export_multiline var text: String
+@export var image: Texture2D
+@export var next: Dialog
---
+++ scripts/dialog_box.gd
@@ -0,0 +1,44 @@
+extends ColorRect
+
+static var instance = self
+
+@onready var text: Label = $Text
+@export var char_time: float
+@export var time_between: float
+var char_timer: float
+
+var dialog: Dialog
+var character: int
+
+func _ready():
+	run(load("res://resources/example.tres"))
+	char_time = char_time / 1000
+
+func _process(delta: float) -> void:
+	print(dialog.text)
+	if not dialog: return
+	char_timer += delta
+	if char_timer >= char_time and character <= dialog.text.length():
+		character += 1
+		char_timer = 0
+		if character >= dialog.text.length():
+			wait_until(dialog)
+			return
+		text.text = dialog.text.substr(0, character + 1)
+
+func wait_until(source: Dialog):
+	await get_tree().create_timer(time_between).timeout
+	if source.next:
+		run(source.next)
+	else:
+		hide()
+
+func run(new: Dialog) -> void:
+	dialog = new
+	character = 0
+	char_timer = 0
+	text.text = ""
+	show()
+
+static func show_dialog(new: Dialog) -> void:
+	instance.run(new)
